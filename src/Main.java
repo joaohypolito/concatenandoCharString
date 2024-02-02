5b0bd4fd35500a395b32e6af8ad84d0b145d1295
João Pedro A. Hypólito <jpazhy@gmail.com>
2024-02-02 13:15:32 -0300

    feat-cria-main
    Cria a classe java main para fazer o exercício proposto

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// Declare uma variável do tipo char (letra) e uma variável do tipo String (palavra). Atribua valores a essas variáveis e concatene-as em uma mensagem.
+
+public class Main {
+    public static void main(String[] args) {
+        char pontuacao = '!';
+        String frase = "Oi, meu nome é Goku";
+
+        System.out.println(frase + pontuacao);
+    }
+}
